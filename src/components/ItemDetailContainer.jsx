35b253abe980b690e820dd03a6a7ec47de087cb9
gabrielguzman <gabrielguzman147.gg@gmail.com>
2022-06-09 10:40:22 -0300

    Detalle de producto entrega correcto

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,29 +1,8 @@
-// import React, { useEffect, useState } from 'react'
 import ItemDetail from './ItemDetail'
-//import { products } from '../data/products'
 import GetITem from './getItem';
 
 
 export default function ItemDetailContainer() {
-    // const [resultado, setResultado ] = useState({});
-
-    // useEffect(() => {
-    //     const getProduct = new Promise((res, rej) => {
-    //       setTimeout(() => {
-    //         res(products[0])/* id: 1,
-    //         nombre: "Apple iPhone 13 Pro Max",
-    //         precio: "200.000",
-    //         imagen: "img/01.jpg",
-    //         colores: "Verde · Rosa · Azul · Azul medianoche · Blanco estelar" */ 
-    //       },2000);
-    //     });
-    
-      //   getProduct
-      //   .then((result)=>{
-      //     setResultado(result);
-      //   })
-      // }, [])
-    
     let resultado = GetITem();
     
     return (
