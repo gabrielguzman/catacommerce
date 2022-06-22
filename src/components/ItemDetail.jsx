ec6575cd569e17a3d6f27a7fbe3f85dfcb9949e5
gabrielguzman <gabrielguzman147.gg@gmail.com>
2022-06-22 07:17:26 -0300

    sugerencia addItem en ondAdd

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -13,41 +13,52 @@ export default function ItemDetail({product}) {
     function addOn(quantityToAdd) {
         //alert(`Se han agregado: ${quantityToAdd} productos`);
         setUnidades(quantityToAdd);
+        addItem(product, quantityToAdd);
     }
     
     return (
-        <>
-            <div className="container bg-light pb-3 box-1 mt-5">
-                <div className="row"> 
-                    <h3 className='mt-3'>Detalle del Producto</h3>
-                    <div className="col-lg-6 col-sm-12 mt-3">
-                        <div className="card">
-                            <img className="card-img-top" src={product.imagen} alt="product" />
-                        </div>
-                    </div>
-                    <div className="col-lg-6 col-sm-12 mt-3">
-                        <div className="alert alert-info">
-                            <h4 className='text-start'>Nombre: {product.nombre}</h4>
-                            <h4 className='text-start'>Categoria: {product.categoria}</h4>
-                            <h4 className='text-start'>Precio: ${product.precio}</h4>
-                        </div> 
-                        <div className="alert alert-info" role="alert">
-                            <h3>Descripción del producto</h3>
-                            {product.descripcion}
-                        </div>
-                        <div className='text-center alert alert-info'>
-                            {unidades > 0 ? 
-                            <div>
-                                <p className='fw-bolder'>*Esta por comprar: Producto: {product.nombre} - Unidades: {unidades}</p>
-                                <Link to={'/cart'} className="btn btn-danger" 
-                                onClick={()=>addItem(product,unidades)}>Comprar</Link>
-                            </div>
-                            :<ItemCount stock={product.stock} initial={1} addOn={addOn}/>} 
-                        </div>
-                        {/* LLamo a ItemCount y le envio props como solicita la diapositiva*/}                       
-                    </div>
-                </div>
+      <>
+        <div className="container bg-light pb-3 box-1 mt-5">
+          <div className="row">
+            <h3 className="mt-3">Detalle del Producto</h3>
+            <div className="col-lg-6 col-sm-12 mt-3">
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src={product.imagen}
+                  alt="product"
+                />
+              </div>
             </div>
-        </>
-  )
+            <div className="col-lg-6 col-sm-12 mt-3">
+              <div className="alert alert-info">
+                <h4 className="text-start">Nombre: {product.nombre}</h4>
+                <h4 className="text-start">Categoria: {product.categoria}</h4>
+                <h4 className="text-start">Precio: ${product.precio}</h4>
+              </div>
+              <div className="alert alert-info" role="alert">
+                <h3>Descripción del producto</h3>
+                {product.descripcion}
+              </div>
+              <div className="text-center alert alert-info">
+                {unidades > 0 ? (
+                  <div>
+                    <p className="fw-bolder">
+                      *Esta por comprar: Producto: {product.nombre} - Unidades:{" "}
+                      {unidades}
+                    </p>
+                    <Link to={"/cart"} className="btn btn-danger">
+                      Terminar mi compra
+                    </Link>
+                  </div>
+                ) : (
+                  <ItemCount stock={product.stock} initial={1} addOn={addOn} />
+                )}
+              </div>
+              {/* LLamo a ItemCount y le envio props como solicita la diapositiva*/}
+            </div>
+          </div>
+        </div>
+      </>
+    );
 }
