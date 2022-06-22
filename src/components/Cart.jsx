738d9ea3158acda6fec30a5093e9a3d8e0433c4b
gabrielguzman <gabrielguzman147.gg@gmail.com>
2022-06-22 09:57:06 -0300

    cambiado forma de renderizar cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,87 +1,87 @@
-import { render } from '@testing-library/react';
 import React from 'react';
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import {CartContext} from '../context/CartContext';
-import Item from './Item';
+
 
 export default function Cart() {
 
   const {cart, getItemPrice, clearCart, removeItem} = useContext(CartContext);
-
-  function renderCarrito(){
-    if(cart.length > 0){
+  
+  const renderCarrito = () => {
       return (
         <div className='container'> 
-          <h3>Carrito</h3>
-          <table className='table'>
-                <thead className='table-info'>
+          <h4 className='mt-3'>Carrito</h4>
+          <table className='table table-striped table-hover mt-3'>
+                <thead>
                   <tr>
-                    <th >
+                    <th scope="col" >
                       Producto
                     </th>
-                    <th>
+                    <th scope="col">
                       Precio
                     </th>
-                    <th>
+                    <th scope="col">
                       Cantidad
                     </th>
-                    <th>
+                    <th scope="col">
                       SubTotal
                     </th>
-                    <th>
+                    <th scope="col">
                       Acciones
                     </th>
                   </tr>
                 </thead>
                 <tbody >
-                  
           {
             cart?.map((item, key) => (
                   <tr key= {key}>
-                    <th>
+                    <td>
                     {item.nombre}
-                    </th>
-                    <th>
+                    </td>
+                    <td>
                     ${item.precio}
-                    </th>
-                    <th>
+                    </td>
+                    <td>
                     {item.quantity}
-                    </th>
-                    <th>
+                    </td>
+                    <td>
                     ${item.quantity * item.precio}
-                    </th>
-                    <th>
-                    <button className='btn btn-danger' onClick={()=>removeItem(item.id)}>X</button>
-                    </th>
+                    </td>
+                    <td>
+                    <button className='btn btn-sm btn-danger' onClick={()=>removeItem(item.id)}>X</button>
+                    </td>
                   </tr>
             ))}
             </tbody>
             <tfoot>
-              <tr>
-                <td className='h5 bg-warning'>Total: ${getItemPrice()}</td>
+              <tr style={{borderWidth: "0"}}>
+                <td colSpan={3}></td>
+                <td className='table-danger fw-bolder '>Total: ${getItemPrice()}</td>
+                <td></td>
               </tr>
             </tfoot>
             </table>
-            <div className="btn-group" role="group" aria-label="Basic example">
-            <Link to={'/'} className='btn btn-primary' >Seguir Comprando</Link>
-            <button className='btn btn-danger' onClick={()=>clearCart()}>Vaciar</button>
+            <div>
+            <Link to={'/'} className='btn btn-primary mt-3 mr-1'>Seguir Comprando</Link>
+            <button className='btn btn-danger mt-3' onClick={()=>clearCart()}>Vaciar</button>
             </div>
         </div> 
       );
-    }else{
-      return (
-      <div>
-        <h5 className='bg-info'>No hay elementos en el carrito</h5>
+  }
+
+  const renderItemVacios = () => {
+    return(
+      <div className='container mt-5'>
+        <h5 className='fs-5 '>No hay elementos en el carrito</h5>
         <Link to={'/'} className='btn btn-primary'>Volver a Inicio</Link>
       </div>
-      );
-    }
-  }
+    )
+  } 
 
   return (
     <>
-      {renderCarrito()}
+      {cart.length > 0 ? renderCarrito() : renderItemVacios()}
     </>
   )
 }
